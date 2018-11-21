78a3056f0b110f7cf18410c52f02913133101293
Juan Ignacio Regueira <jregueira95@gmail.com>
2018-11-21 00:07:18 -0300

    Agregamos FOSUserBundle

--- src/ProductoBundle/Form/CategoryApiType.php
+++ src/ProductoBundle/Form/CategoryApiType.php
@@ -5,25 +5,10 @@ namespace ProductoBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Lenght;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class CategoryApiType extends CategoryType
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-        ->add('myField', TextType::class, array(
-            'required' => true,
-            'constraints' => array(new Length(array('min' => 3)))
-        ));
-    }
-    /**
-     * {@inheritdoc}
-     */
+
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
--- src/ProductoBundle/Form/ProductoApiType.php
+++ src/ProductoBundle/Form/ProductoApiType.php
@@ -8,15 +8,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductoApiType extends ProductoType
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder->add('name')->add('price')->add('stock')->add('categorias');
-    }/**
-     * {@inheritdoc}
-     */
+ 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
